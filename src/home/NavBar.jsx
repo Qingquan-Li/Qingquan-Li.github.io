a833b40b9d89eb0c0ff61ff0df4cd25cde047997
Qingquan Li <liqqisme@gmail.com>
2023-05-27 13:50:55 -0400

    Add LinkedIn link

--- src/home/NavBar.jsx
+++ src/home/NavBar.jsx
@@ -27,6 +27,17 @@ export default function NavBar() {
                   <path fillRule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"/>
                 </svg>
               </a>
+              <a className="nav-link"
+                href="https://www.linkedin.com/in/qingquan-li/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                LinkedIn&nbsp;
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
+                  <path fillRule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"/>
+                  <path fillRule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"/>
+                </svg>
+              </a>
               {/* <a className="nav-link" href="resume.html" target="_blank">Resume&nbsp; */}
               <Link to="/resume" className="nav-link" target="_blank" rel="noopener noreferrer">
                 Resume&nbsp;
